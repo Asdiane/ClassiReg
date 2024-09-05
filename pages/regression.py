62024f750c91b42c9e9dc0bfb0e826a4c141c1a3
AhmedDianeSek <AhmedDiane@teccart.online>
2024-09-05 01:51:49 -0400

    Les modèles de classification

--- pages/regression.py
+++ pages/regression.py
@@ -1,12 +1,16 @@
 import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+import xgboost as xgb
 import matplotlib.pyplot as plt
 import joblib
 import io
+import pandas as pd
 
 def regression_page():
     if 'data' in st.session_state:
@@ -22,53 +26,71 @@ def regression_page():
         # Sélection de l'algorithme
         st.write("### Sélectionner un algorithme de Régression")
         algorithm = st.selectbox("Choisissez un algorithme", 
-                                 ("Régression Linéaire", "Régression Polynomiale", "Forêt Aléatoire", "Ridge", "Lasso", "ElasticNet"))
+                                 ("Régression Linéaire", "Régression Polynomiale", "Forêt Aléatoire", 
+                                  "Ridge", "Lasso", "ElasticNet", "Réseau de Neurones", "Gradient Boosting", "XGBoost"))
 
         # Option pour charger un modèle existant
         load_model = st.checkbox("Charger un modèle existant")
 
-        model = None
+        # Stockage du modèle et du scaler dans st.session_state pour éviter la réinitialisation
+        if 'model' not in st.session_state:
+            st.session_state.model = None
+        if 'scaler' not in st.session_state:
+            st.session_state.scaler = None
+        if 'pca' not in st.session_state:
+            st.session_state.pca = None
+
         if load_model:
             model_file = st.file_uploader("Téléchargez le fichier du modèle", type=["pkl"])
             if model_file is not None:
-                model = joblib.load(model_file)
+                st.session_state.model = joblib.load(model_file)
                 st.success("Modèle chargé avec succès.")
 
-        if not load_model or model is None:
+        if not load_model and st.session_state.model is None:
             if st.button("Entraîner le modèle"):
                 X = data[feature_columns]
                 y = data[target_column]
 
+                # Normaliser les données
+                st.session_state.scaler = StandardScaler()
+                X_scaled = st.session_state.scaler.fit_transform(X)
+
+                # Réduction des caractéristiques avec PCA
+                st.session_state.pca = PCA(n_components=min(len(feature_columns), 10))  # Garder un maximum de 10 composantes principales
+                X_reduced = st.session_state.pca.fit_transform(X_scaled)
+
                 # Diviser les données en ensembles d'entraînement et de test
-                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+                X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.3, random_state=42)
 
                 # Initialiser le modèle
                 if algorithm == "Régression Linéaire":
-                    model = LinearRegression()
+                    st.session_state.model = LinearRegression()
                 elif algorithm == "Régression Polynomiale":
                     poly = PolynomialFeatures(degree=2)
                     X_train_poly = poly.fit_transform(X_train)
                     X_test_poly = poly.transform(X_test)
-                    model = LinearRegression()
-                    model.fit(X_train_poly, y_train)
-                    y_pred = model.predict(X_test_poly)
+                    st.session_state.model = LinearRegression()
+                    st.session_state.model.fit(X_train_poly, y_train)
+                    y_pred = st.session_state.model.predict(X_test_poly)
                 elif algorithm == "Forêt Aléatoire":
-                    model = RandomForestRegressor()
-                    model.fit(X_train, y_train)
-                    y_pred = model.predict(X_test)
+                    st.session_state.model = RandomForestRegressor()
                 elif algorithm == "Ridge":
-                    model = Ridge()
+                    st.session_state.model = Ridge()
                 elif algorithm == "Lasso":
-                    model = Lasso()
+                    st.session_state.model = Lasso()
                 elif algorithm == "ElasticNet":
-                    model = ElasticNet()
+                    st.session_state.model = ElasticNet()
+                elif algorithm == "Réseau de Neurones":
+                    st.session_state.model = MLPRegressor(hidden_layer_sizes=(100,), max_iter=500)
+                elif algorithm == "Gradient Boosting":
+                    st.session_state.model = GradientBoostingRegressor()
+                elif algorithm == "XGBoost":
+                    st.session_state.model = xgb.XGBRegressor()
 
-                if algorithm not in ["Régression Polynomiale", "Ridge", "Lasso", "ElasticNet"]:
-                    model.fit(X_train, y_train)
-                    y_pred = model.predict(X_test)
-                elif algorithm in ["Ridge", "Lasso", "ElasticNet"]:
-                    model.fit(X_train, y_train)
-                    y_pred = model.predict(X_test)
+                # Entraîner le modèle (sauf pour la régression polynomiale qui est déjà entraînée)
+                if algorithm not in ["Régression Polynomiale"]:
+                    st.session_state.model.fit(X_train, y_train)
+                    y_pred = st.session_state.model.predict(X_test)
 
                 # Évaluer le modèle
                 mse = mean_squared_error(y_test, y_pred)
@@ -91,7 +113,7 @@ def regression_page():
                 model_filename = st.text_input("Nom du fichier pour sauvegarder le modèle", "model.pkl")
                 if model_filename:  # Vérifiez que le nom du fichier est renseigné
                     buffer = io.BytesIO()
-                    joblib.dump(model, buffer)
+                    joblib.dump(st.session_state.model, buffer)
                     buffer.seek(0)  # Revenir au début du buffer
 
                     # Utiliser st.download_button pour permettre le téléchargement
@@ -103,5 +125,42 @@ def regression_page():
                     )
                 else:
                     st.error("Veuillez entrer un nom de fichier valide pour télécharger le modèle.")
+
+        # Section pour faire des prédictions en temps réel
+        st.write("### Prédictions en Temps Réel")
+        if st.session_state.model:
+            user_input = {}
+            for col in feature_columns:
+                min_val = float(data[col].min())
+                max_val = float(data[col].max())
+                mean_val = float(data[col].mean())
+                user_input[col] = st.slider(
+                    f"Réglez la valeur pour {col}", 
+                    min_value=min_val, 
+                    max_value=max_val, 
+                    value=mean_val
+                )
+
+            input_df = pd.DataFrame([user_input])
+            st.write("### Vos entrées")
+            st.write(input_df)
+
+            # Appliquer la même transformation aux nouvelles données d'entrée
+            input_df_scaled = st.session_state.scaler.transform(input_df)
+            input_df_reduced = st.session_state.pca.transform(input_df_scaled)
+
+            if 'prediction' not in st.session_state:
+                st.session_state.prediction = None
+
+            # Prédiction
+            if st.button("Faire une Prédiction"):
+                prediction = st.session_state.model.predict(input_df_reduced)
+                st.session_state.prediction = prediction[0]
+
+            # Afficher la prédiction si elle existe
+            if st.session_state.prediction is not None:
+                st.write("### Prédiction")
+                st.write(f"La valeur prédite est : {st.session_state.prediction:.2f}")
+
     else:
         st.warning("Veuillez d'abord télécharger un fichier pour commencer la régression.")
